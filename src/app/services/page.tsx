e9264306372e8d934cdade3f193d026bd93cd570
mqnoy <qnoy.rifky@gmail.com>
2024-06-08 10:15:46 +0700

    wrap all to section

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,7 +1,15 @@
 import { FC } from "react";
 
 const ServicePage: FC = () => {
-  return <div>ServicePage page</div>;
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          under construction
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ServicePage;
